c804bfa8b3ee7ee3e07b5493e1b4008ad6bd5665
Quentin Mulot <75835131+Masterkint1982@users.noreply.github.com>
2021-03-03 09:55:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,49 @@
+# Type your code here
 n=int(input('please enter an integer between 1 and 9999: '))
 
-number_list_one = ["","one","two","three","four","five","six","seven","eight","nine"]
-number_list_two = ["ten","eleven","twelve","thirteen","fourteen","fifteen","seventeen","eighteen","nineteen"]
-number_list_three = ["","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"]
-number_list_four = ["hundred"]
+# Main program
+# Defintion of the dictionary
 
-if n < 10:
-    convert_to_word = number_list_one[n]
-    print(convert_to_word)
-elif n >= 10 and n <20:
-    n = n - 10
-    convert_to_word = number_list_two[n]
-    print(convert_to_word)
-elif n >=20 and n <100:
-    n1 = n % 10
-    n2 = ((n -n1)-1) //10
-    convert_to_word1 = number_list_one[n1]
-    convert_to_word2 = number_list_three[n2]
-    print(convert_to_word2,convert_to_word1, sep=" ")
-elif n >=100 and <=999:
-    n1 = 0
-    n2 = n //100
-    n3 = 
-    n4 = 
-    convert_to_word1 = number_list_one[n2]
-    convert_to_word2 = number_list_four[n1]
-    print(convert_to_word1, convert_to_word2, sep= " ")
+single_digit = { 0: "zero", 1 : "one", 2 : "two", 3 : "three", 4 : "four", 5 : "five",
+ 6 : "six", 7 : "seven", 8 : "eight", 9 : "nine"}
+
+double_digit = { 10 : "ten", 11 : "eleven", 12 : "twelve", 13 : "thirteen",
+ 14 : "fourteen", 15 : "fifteen", 16 : "sixteen"}
+
+tens_digit = { 20 : "twenty", 30 : "thirty", 40 : "forty" , 50 : "fifty" , 60 : "sixty"
+70 : "seventy", 80 : "eighty", 90 : "ninety"}
+
+hundreds_digit = { 100 : "hundred", 1000 : "thousand"}
+
+
+def single_digit_word (number):
+    if number >=0 and number <10:
+        return single_digit[number]
+
+def double_digit_word (number_1):
+    if number_1 >= 10 and number_1 < 20:
+        return double_digit[number_1]
+
+    if number_1 >= 20 and number_1 < 100:
+        index = number_1 % 10
+        index_1 = number_1 // 10
+        if index != 0 :
+            return tens_digit[index_1] + single_digit[index]
+        else :
+            return tens_digit[number_1]
+
+def tens_digit_word(number_1):
+    if number_1 >=100 and number_1 < 1000:
+         index = number_1 % 100
+         index_1 = number_1 // 100
+         if index != 0 :
+            if index < 10:
+                return single_digit_word(index_1) + hundreds_digit(0) + single_digit_word(index)
+            elif index < 100:
+                return single_digit_word(index_1) + hundreds_digit(0) + double_digit_word(index)
+            else :
+                return single_digit_word(index_1)
+
+
+
+# Test the code
